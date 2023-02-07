729a4dcefb5f49d6a7db1d42bd3fc9d0484cc7d2
Elza Contiero <contieroelza@gmail.com>
2023-02-07 16:38:36 +0000

    Add ISerialiser

---
+++ SampleProject/src/main/java/com/sparta/model/ISerialiser.java
@@ -0,0 +1,12 @@
+package com.sparta.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
+
+public interface ISerialiser {
+    public String employeesToString(Employee[] emps) throws JsonProcessingException;
+    public Employee[] stringToEmployees(String s) throws JsonProcessingException;
+    public void employeesToFile(String pathname, Employee[] emps) throws IOException;
+    public Employee[] fileToEmployees(String pathname) throws IOException;
+}
+
